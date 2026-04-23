9580568d002fd141cebdabaaa3a5aa05ae3b11be
moara <8263241+moaradin@users.noreply.github.com>
2026-04-23 17:04:36 -0400

    cachyos

--- modules/hosts/myMachine/configuration.nix
+++ modules/hosts/myMachine/configuration.nix
@@ -10,13 +10,13 @@
       self.nixosModules.nvidia
       self.nixosModules.gaming
       self.nixosModules.mounts
-      #self.nixosModules.cachyos
+      self.nixosModules.cachyos
       
     ];
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+    #boot.kernelPackages = pkgs.linuxPackages_latest;
 
     networking.hostName = "myMachine";
     networking.networkmanager.enable = true;
