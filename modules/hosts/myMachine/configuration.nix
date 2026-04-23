22f868bc44605100a8e5a702c9b6458686e7f889
moara <8263241+moaradin@users.noreply.github.com>
2026-04-23 10:50:44 -0400

    update

--- modules/hosts/myMachine/configuration.nix
+++ modules/hosts/myMachine/configuration.nix
@@ -126,6 +126,15 @@
       fishPlugins.z
       grc
       fzf
+      
+      # Gstreamer
+      gst_all_1.gstreamer
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-ugly
+      gst_all_1.gst-libav
+      gst_all_1.gst-vaapi
     ];
 
     fonts = {
