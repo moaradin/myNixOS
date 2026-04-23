f34bd9da319d31ac61fa1d0c8b069f9c3e58e3cb
moara <8263241+moaradin@users.noreply.github.com>
2026-04-23 07:34:02 -0400

    niri update

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -88,7 +88,7 @@
           };
           mouse.accel-profile = "flat";
           # DISABLED: To avoid potential inline property wrapper bug
-          # focus-follows-mouse.max-scroll-amount = "0%";
+          focus-follows-mouse.max-scroll-amount = "0%";
         };
 
         outputs = {
@@ -99,7 +99,7 @@
             # position = { x = 1920; y = 0; };
             
             focus-at-startup = {};
-            variable-refresh-rate = {};
+            variable-refresh-rate.on-demand = true;
             
             hot-corners.off = {};
           };
