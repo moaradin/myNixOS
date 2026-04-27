90fed8a3f9c73b9fc1e67beda9072478e405fd44
moara <8263241+moaradin@users.noreply.github.com>
2026-04-27 06:20:35 -0400

    update

--- modules/features/nvf.nix
+++ modules/features/nvf.nix
@@ -11,9 +11,9 @@
     programs.nvf = {
       enable = true;
       settings = {
-        vim.theme.enable = true;
-        vim.theme.name = "gruvbox";
-        vim.theme.style = "dark";
+      #  vim.theme.enable = true;
+      #  vim.theme.name = "gruvbox";
+      #  vim.theme.style = "dark";
 
         vim.languages.nix.enable = true;
       };
