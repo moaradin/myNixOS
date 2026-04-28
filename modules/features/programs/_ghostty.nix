0ab96150384af8886d36d4b7a09992c9ef2ce2fa
moara <8263241+moaradin@users.noreply.github.com>
2026-04-28 13:05:51 -0400

    update

--- modules/features/programs/_ghostty.nix
+++ modules/features/programs/_ghostty.nix
@@ -1,10 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   home-manager.users.moara.programs.ghostty = {
     enable = true;
     enableFishIntegration = true;
     settings = {
       command = "${pkgs.fish}/bin/fish";
-      theme = "noctalia";
+      theme = "Gruvbox Dark";
       background-opacity = 0.80;
       clipboard-read = "allow";
       clipboard-write = "allow";
