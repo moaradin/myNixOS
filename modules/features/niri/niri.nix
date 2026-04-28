c75224849b0fd22f0723aff3da0bf72c266e9cee
moara <8263241+moaradin@users.noreply.github.com>
2026-04-28 05:31:56 -0400

    update

--- modules/features/niri/niri.nix
+++ modules/features/niri/niri.nix
@@ -41,7 +41,11 @@
         in
         {
 
-          xdg.configFile."niri/config.kdl".text = builtins.readFile ./config.kdl;
+          xdg.configFile."niri/config.kdl".text =
+            builtins.replaceStrings
+              [ "spawn \"niri-screenshot\"" ]
+              [ "spawn \"${niri-screenshot}/bin/niri-screenshot\"" ]
+              (builtins.readFile ./config.kdl);
 
           home.pointerCursor = {
             name = "Bibata-Modern-Ice";
@@ -94,6 +98,8 @@
             templates = "/home/moara/Templates";
             videos = "/home/moara/Videos";
           };
+
+          home.packages = [ niri-screenshot ];
 
         };
 
