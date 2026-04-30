acdb29eb95bd5eedfbcf5c73cb84b883cffc300d
moara <8263241+moaradin@users.noreply.github.com>
2026-04-30 17:35:06 -0400

    update

--- modules/features/nvidia.nix
+++ modules/features/nvidia.nix
@@ -30,7 +30,14 @@
       hardware.nvidia = {
         open = true;
         modesetting.enable = true;
-        package = config.boot.kernelPackages.nvidiaPackages.latest;
+        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "595.71.05";
+          sha256_64bit = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
+          sha256_aarch64 = "sha256-XzKloS00dFKTd4ATWkTIhm9eG/OzR/Sim6MboNZWPu8=";
+          openSha256 = "sha256-Lfz71QWKM6x/jD2B22SWpUi7/og30HRlXg1kL3EWzEw=";
+          settingsSha256 = "sha256-mXnf3jyvznfB3OfKd657rxv0rYHQb/dX/Riw/+N9EKU=";
+          persistencedSha256 = "sha256-Z/6IvEEa/XfZ5F5qoSIPvXJLGtscYVqjFxHZaN/M2Ts=";
+        };
         powerManagement.enable = true;
         # Required for VA-API to work with open modules (driver 595+)
         videoAcceleration = true;
