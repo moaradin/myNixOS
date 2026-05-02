eff504c457f2c4de347a27bae2831be3697a870a
moara <8263241+moaradin@users.noreply.github.com>
2026-05-01 20:05:31 -0400

    update

--- modules/features/gaming.nix
+++ modules/features/gaming.nix
@@ -63,8 +63,6 @@
           version = "nvdia-blackwell-patched";
         });
         enable = true;
-        capSysNice = true;
-
       };
 
       # ── Steam ─────────────────────────────────────────────────────────────
@@ -78,23 +76,6 @@
         extraPackages = with pkgs; [
           libsecret
         ];
-      };
-
-      programs.steam.package = pkgs.steam.override {
-        extraPkgs =
-          pkgs': with pkgs'; [
-            libXcursor
-            libXi
-            libXinerama
-            libXScrnSaver
-            libpng
-            libpulseaudio
-            libvorbis
-            stdenv.cc.cc.lib # Provides libstdc++.so.6
-            libkrb5
-            keyutils
-            # Add other libraries as needed
-          ];
       };
 
       # ── Services ────────────────────────────────────────────────────────────
