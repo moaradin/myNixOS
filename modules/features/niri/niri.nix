8301c501a264ab64dff189e159a40bb5892f7eb6
moara <8263241+moaradin@users.noreply.github.com>
2026-05-02 10:35:44 -0400

    update

--- modules/features/niri/niri.nix
+++ modules/features/niri/niri.nix
@@ -47,25 +47,14 @@
           };
 
           gtk = {
-
             enable = true;
             iconTheme = {
-
               name = "Gruvbox-Plus-Dark";
-<<<<<<< Updated upstream
               package = pkgs.gruvbox-plus-icons;
-=======
-
-              package = pkgs.gruvbox-plus-icons;
-
->>>>>>> Stashed changes
             };
             theme = {
-
               name = "gruvbox-dark";
-
               package = pkgs.gruvbox-dark-gtk;
-
             };
             gtk4.theme = null;
           };
