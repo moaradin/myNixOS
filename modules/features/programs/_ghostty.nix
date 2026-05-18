cece1eebd5a5bda114216fe92740e4643d89c465
moara <8263241+moaradin@users.noreply.github.com>
2026-05-18 12:04:09 -0400

    update

--- modules/features/programs/_ghostty.nix
+++ modules/features/programs/_ghostty.nix
@@ -7,6 +7,7 @@
       command = "${pkgs.fish}/bin/fish";
       theme = "Gruvbox Dark";
       background-opacity = 0.80;
+      background-blur-radius = 25;
       clipboard-read = "allow";
       clipboard-write = "allow";
       copy-on-select = "clipboard";
