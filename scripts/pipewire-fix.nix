f86f516cae2ae29d8172531757751142578c556c
moara <8263241+moaradin@users.noreply.github.com>
2026-05-24 19:44:23 -0400

    update

---
+++ scripts/pipewire-fix.nix
@@ -0,0 +1,21 @@
+{ ... }:
+{
+
+services.pipewire.extraConfig.pipewire = {
+  "98-crackling-fix" = {
+    "context.properties" = {
+      "default.clock.quantum" = 1024;
+      "default.clock.min-quantum" = 1024;
+      "default.clock.max-quantum" = 8192;
+    };
+  };
+};
+
+# additional fix for very bad devices or VM. 
+services.pipewire.wireplumber.extraConfig = {
+  "99-crackling-fix" = {
+    "api.alsa.period-size" = 1024;
+    "api.alsa.headroom" = 8192;
+  };
+};
+
