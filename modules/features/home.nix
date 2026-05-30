380e736bd73f28987365bb09a9fac105fe9d65cd
moara <8263241+moaradin@users.noreply.github.com>
2026-05-30 17:49:44 -0400

    update

--- modules/features/home.nix
+++ modules/features/home.nix
@@ -237,6 +237,18 @@
               };
           };
 
+          programs.yt-dlp = {
+            settings = {
+              embed-thumbnail = true;
+              embed-subs = true;
+              sub-langs = "all";
+              downloader = "aria2c";
+              downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
+              paths = "~/Downloads";
+              output = "%(title)s.%(ext)s";
+            };
+          };
+
           # Example: You can optionally move your Git config here from configuration.nix
           programs.git = {
             enable = true;
