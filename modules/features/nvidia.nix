90be8b77fe8a857e91aeb0aa440f7920dfe8513b
moara <8263241+moaradin@users.noreply.github.com>
2026-07-07 16:45:05 -0400

    update

--- modules/features/nvidia.nix
+++ modules/features/nvidia.nix
@@ -30,15 +30,15 @@
       hardware.nvidia = {
         open = true;
         modesetting.enable = true;
-        # package = config.boot.kernelPackages.nvidiaPackages.new_feature;
-        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          version = "610.43.02";
-          sha256_64bit = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
-          sha256_aarch64 = "sha256-isWTnokUA/dzWocFBLalnk4+O5gSExVjs3dVpdYTU88=";
-          openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
-          settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
-          persistencedSha256 = "sha256-Whgv9X+v2fRhzliOl2LzltY9v1SxDafFfv3IUPqj/hk=";
-        };
+        package = config.boot.kernelPackages.nvidiaPackages.production;
+        #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        #  version = "610.43.02";
+        #  sha256_64bit = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
+        #  sha256_aarch64 = "sha256-isWTnokUA/dzWocFBLalnk4+O5gSExVjs3dVpdYTU88=";
+        #  openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
+        #  settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
+        #  persistencedSha256 = "sha256-Whgv9X+v2fRhzliOl2LzltY9v1SxDafFfv3IUPqj/hk=";
+        #};
         powerManagement.enable = true;
         # Required for VA-API to work with open modules (driver 595+)
         videoAcceleration = true;
